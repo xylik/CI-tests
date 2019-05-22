c70710655ef048a0af17222d48207236fb8ae4f0
Igor Armus <igor.armus@gmail.com>
2019-05-22 21:19:25 +0200

    close anr dialog added gradlew shell printing corrected

---
+++ scripts/close-anr.sh
@@ -0,0 +1,7 @@
+sleep 700 #estimated time before instrumentation test starts running
+# Send keystroke Arrow Right
+sleep 3; adb shell input keyevent 22
+# Send keystroke Arrow Right again
+sleep 3; adb shell input keyevent 22
+# Send keystroke Enter to press a button on the dialog
+sleep 3; adb shell input keyevent 66
