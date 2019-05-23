f1f9ba757cda1909f9dd942613ed5eddb85994e8
Igor Armus <igor.armus@gmail.com>
2019-05-23 13:43:27 +0200

    added print statement to close-anr.sh so travis doesn't terminate machine because of inactivity added gpu info print

--- scripts/close-anr.sh
+++ scripts/close-anr.sh
@@ -1,4 +1,8 @@
-sleep 600 #estimated time for emulator to completly boot up
+for i in {1..20}
+do
+    echo "Waiting for emulator"
+    sleep 30 #20*30=600s estimated time for emulator to completely boot up
+done
 # Send keystroke Arrow Right
 sleep 3; adb shell input keyevent 22
 # Send keystroke Arrow Right again
