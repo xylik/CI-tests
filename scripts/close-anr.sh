8651bfd850f2bcc832a337c09790381be0c5262d
Igor Armus <igor.armus@gmail.com>
2019-05-27 10:08:37 +0200

    api 22 arm added

--- scripts/close-anr.sh
+++ scripts/close-anr.sh
@@ -1,11 +1,14 @@
-for i in {1..20}
-do
-    echo "Waiting for emulator"
-    sleep 30 #20*30=600s estimated time for emulator to completely boot up
-done
+#!/bin/bash
+#for i in {1..20}
+#do
+#    echo "Waiting for emulator"
+#    sleep 30 #20*30=600s estimated time for emulator to completely boot up
+#done
+echo "Closing ANR"
 # Send keystroke Arrow Right
 sleep 3; adb shell input keyevent 22
 # Send keystroke Arrow Right again
 sleep 3; adb shell input keyevent 22
 # Send keystroke Enter to press a button on the dialog
 sleep 3; adb shell input keyevent 66
+echo "ANR dialog closed"
